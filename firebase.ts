3de2b6189b6a97ef506a837feb684a02dcd09a7d
Adarsh L K <129508819+adarshlkdev@users.noreply.github.com>
2024-03-31 05:39:31 +0530

    Update firebase.ts for configuration 🤩

--- firebase.ts
+++ firebase.ts
@@ -4,10 +4,10 @@ import {getStorage} from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyACIrICqn0nHPaS93nMU5SqXbhw8HXj5yY",
-  authDomain: "firesink-23bfc.firebaseapp.com",
-  projectId: "firesink-23bfc",
-  storageBucket: "firesink-23bfc.appspot.com",
+  apiKey: "Your_api_key",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
   messagingSenderId: "482207260584",
   appId: "1:482207260584:web:1c344187c262ea0ccf879d"
 };
